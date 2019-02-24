8fe9a511ad5db0ad81ad606281f12c1b6385c06c
unknown <shaikhubaid769@gmail.com>
2019-02-25 00:15:47 +0530

    Added animation feature

--- contentscript.js
+++ contentscript.js
@@ -3,23 +3,26 @@ var Division= document.createElement("IFRAME");
 Division.className = "Mview";
 document.body.appendChild(Division);
 var box=document.querySelector('.Mview');
-
+box.style.transition = "all 1.5s";
 
 for(var i=0;i<myDiv.length;i++)
 {
     myDiv[i].addEventListener("mouseover", myFunction);
-    myDiv[i].addEventListener("mouseout", function(){box.style.display="none";});
 }
-box.addEventListener("mouseover", function(){this.style.display="block";});
-box.addEventListener("mouseout", function(){this.style.display="none";});
 
 function myFunction()
 {
     var position = this.getBoundingClientRect();
-    var x = Number(position.width)+Number(position.left)-125;
+    var x = Number(position.width)+Number(position.left);
     var y = Number(position.top) + Number(window.scrollY)-175;
+    if(x>620) x-=100;
     box.style.left=String(x)+"px";
     box.style.top=String(y)+"px";
     box.src=this.href;
-    box.style.display="block";
+    var timer = setTimeout("slide()",750); 
+	this.onmouseout = function() {  clearTimeout(timer); } 
+}
+function slide()
+{
+	box.style.display="block";
 }
